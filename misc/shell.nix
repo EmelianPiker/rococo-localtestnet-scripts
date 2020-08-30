e0e47a533afc977873067d4fea7190621657d0f4
Evgeni Zhukov <zhukov@soramitsu.co.jp>
2020-08-30 23:35:05 +0700

    q

--- misc/shell.nix
+++ misc/shell.nix
@@ -2,5 +2,5 @@
 pkgs.mkShell {
   #buildInputs = with pkgs; [ pkgconfig gmp protobuf llvm clang openssl pkgsi686Linux.glibc.dev ];
   #buildInputs = with pkgs; [ pkgconfig gmp protobuf llvm clang openssl glibc ];
-  buildInputs = with pkgs; [ protobuf llvm clang ];
+  buildInputs = with pkgs; [ protobuf llvm clang pkgconfig openssl ];
 }
