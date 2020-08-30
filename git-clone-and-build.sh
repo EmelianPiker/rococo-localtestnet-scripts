0075a68e34ce810d3450713a4f83d719d3f8e7ad
Evgeni Zhukov <zhukov@soramitsu.co.jp>
2020-08-30 17:35:31 +0700

    q

--- git-clone-and-build.sh
+++ git-clone-and-build.sh
@@ -4,21 +4,21 @@ top=$PWD
 cd ..
 
 function clone_and_build() {
-	if [ ! -d $1 ]; then
-		git clone $2
-		mv `basename $2` $1
-		pushd $1
-			git checkout $3
+	if [ ! -d $2 ]; then
+		git clone $3
+		mv `basename $3` $2
+		pushd $2
+			git checkout $4
 			ln -s $top/misc/Makefile .
 			ln -s $top/misc/shell.nix .
 			ln -s $top/misc/nix-env.sh .
-			make || exit
+			make $1 || exit
 		popd
 	fi
 }
 
-clone_and_build api       https://github.com/polkadot-js/api 7ecd00a4
-clone_and_build iroha     https://github.com/EmelianPiker/iroha ae9f93f0
-clone_and_build polkadot  https://github.com/paritytech/polkadot fd4b176f
-clone_and_build parachain https://github.com/EmelianPiker/substrate-iroha-bridge-node 5eb92b17
+clone_and_build yarn  api       https://github.com/polkadot-js/api 7ecd00a4
+clone_and_build cargo iroha     https://github.com/EmelianPiker/iroha ae9f93f0
+clone_and_build cargo polkadot  https://github.com/paritytech/polkadot fd4b176f
+clone_and_build cargo parachain https://github.com/EmelianPiker/substrate-iroha-bridge-node 5eb92b17
 
